53f649abc80e015ad3796586b4f34274f455932f
Steven <steven.686295@gmail.com>
2021-07-26 16:36:56 -0400

    Completed PracticeProgramming #1

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/core/ProjectGutenberg.java
@@ -0,0 +1,22 @@
+/*
+ * @author Steven Nguyen
+ * @email: steven.686295@gmail.com
+ * @date: 26 Jul 2021
+ */
+package com.sg.foundations.basics.core;
+
+/**
+ *
+ * @author Steven
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        System.out.println("Did you know that in 1440 (or thereabouts),");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.println("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.println("We are like a modern Gutenberg,");
+        System.out.println("printing vast amounts to the waiting console with ease.");
+    }
+}
