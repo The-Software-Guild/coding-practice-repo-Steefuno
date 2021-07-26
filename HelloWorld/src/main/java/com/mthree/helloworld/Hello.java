4e0fa2b2aa200ab49485a255629b97b39be29262
Steven <steven.686295@gmail.com>
2021-07-26 16:36:56 -0400

    Completed ProacticeProgramming #1

--- HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
+++ HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -1,11 +1,10 @@
 /**
- *@author Your Name
- *email: steven.686295@gmail.com
- *date: 26 July 2021
- *purpose: My first Hello World program in an IDE
+ *@author Steven Nguyen
+ *@email: steven.686295@gmail.com
+ *@date: 26 July 2021
+ *My first Hello World program in an IDE
  */
 
-// Use YOUR Group Id value here if it is different from com.mthree
 package com.mthree.helloworld; 
 
 public class Hello {
