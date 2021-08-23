83c5db66b5270511c00e14864f319b1726fde688
Steven <steven.686295@gmail.com>
2021-08-23 14:34:54 -0400

    Completed jQuery Browser Events Exercise

--- jQuery-Browser-Events/js/home.js
+++ jQuery-Browser-Events/js/home.js
@@ -1,3 +1,9 @@
+let cityButtons = {
+    "akronButton": "akron",
+    "minneapolisButton": "minneapolis",
+    "louisvilleButton": "louisville"
+};
+
 function display(id) {
     $("#pageContent")
         .children()
@@ -7,32 +13,57 @@ function display(id) {
 }
 
 function onDocumentReady() {
+    // initialize
     display("#mainInfoDiv");
 
-    // When a button is pressed, display the respective div
+    // when the main button is clicked, display the main menu
     $("#mainButton").on(
         "click",
         function() {
             display("#mainInfoDiv");
         }
     );
-    $("#akronButton").on(
-        "click",
+
+    // when a <tr> is hovered over, highlight
+    $("tr").hover(
         function() {
-            display("#akronInfoDiv");
+            $(this).css(
+                {"background-color": "WhiteSmoke"}
+            );
+        },
+        function() {
+            $(this).css(
+                {"background-color": "white"}
+            )
         }
     );
-    $("#minneapolisButton").on(
-        "click",
-        function() {
-            display("#minneapolisInfoDiv");
-        }
-    );
-    $("#louisvilleButton").on(
-        "click",
-        function() {
-            display("#louisvilleInfoDiv");
-        }
-    );
+    
+    // setup each city tab
+    for(let key in cityButtons) {
+        let weatherDiv = "#" + cityButtons[key] + "Weather";
+        let button = "#" + key;
+        let infoDiv = "#" + cityButtons[key] + "InfoDiv";
+        let weatherButton = "#" + cityButtons[key] + "WeatherButton";
+
+        // initially hide the weather info
+        $(weatherDiv).hide();
+
+        // when a city button is clicked, display the respective city and hide the weather
+        $(button).on(
+            "click",
+            function() {
+                $(weatherDiv).hide(0);
+                display(infoDiv);
+            }
+        );
+
+        // toggle weather info on weather button clicked
+        $(weatherButton).on(
+            "click",
+            function() {
+                $(weatherDiv).toggle(400);
+            }
+        );
+    }
 }
 $(document).ready(onDocumentReady);
