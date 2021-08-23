5e7eb6895d003f13d26fc7179c1825cd5007430d
Steven <steven.686295@gmail.com>
2021-08-23 13:51:01 -0400

    Completed jQuery Browser Events

--- jQuery-Browser-Events/js/home.js
+++ jQuery-Browser-Events/js/home.js
@@ -1,3 +1,38 @@
-$(document).ready(function () {
-    
-});
+function display(id) {
+    $("#pageContent")
+        .children()
+        .hide()
+    ;
+    $(id).show()
+}
+
+function onDocumentReady() {
+    display("#mainInfoDiv");
+
+    // When a button is pressed, display the respective div
+    $("#mainButton").on(
+        "click",
+        function() {
+            display("#mainInfoDiv");
+        }
+    );
+    $("#akronButton").on(
+        "click",
+        function() {
+            display("#akronInfoDiv");
+        }
+    );
+    $("#minneapolisButton").on(
+        "click",
+        function() {
+            display("#minneapolisInfoDiv");
+        }
+    );
+    $("#louisvilleButton").on(
+        "click",
+        function() {
+            display("#louisvilleInfoDiv");
+        }
+    );
+}
+$(document).ready(onDocumentReady);
